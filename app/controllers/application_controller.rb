a8ea04a8e76fd90e425868d40181cad302dfb3c3
Uladzislau Dzemianovich <zaptot@gmail.com>
2019-03-30 15:52:50 +0300

    create cards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :categories_list
 
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
