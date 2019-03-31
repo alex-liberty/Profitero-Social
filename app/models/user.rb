db04bf0dddbe5d79eb33ae549c14a229ac828574
Alexandr Dubitsky <alexandr.dubitsky@gmail.com>
2019-03-31 09:49:33 +0300

    add avatars and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
-    #return nil if auth.info.hd["profitero.com"].nil?
+    return nil if auth.info.hd["profitero.com"].nil?
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.email = auth.info.email
+      user.avatar = auth.info.picture
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
